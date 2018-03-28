4eb4897171974fffa17c3f2faab7dbc01bbd9942
sato <super80@gmail.com>
2018-03-29 02:17:14 +0900

    added 04_v_for

---
+++ 04_v_for/index.js
@@ -0,0 +1,12 @@
+'use_strict';
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+});
