6ad752aba2e24734704abbd50b7e4ae4c4597942
sato <super80@gmail.com>
2018-03-31 22:20:52 +0900

    modified 13_conditional

--- 14_list/index.js
+++ 14_list/index.js
@@ -3,6 +3,27 @@
 const vm = new Vue({
     el: '#app',
     data: {
-
+        commonMessage: 'メッセージ',
+        items: [
+            {message: "hello world"},
+            {message: "こんにちは、世界"}
+        ],
+        flight: {
+            flightNumber: 'ZZ999',
+            departureDate: '10APR18',
+            arrivalDate: '10APR18',
+            departureTime: '1700',
+            arrivalTime: '0830',
+            departureAirport: 'HND',
+            arrivalAirport: 'SFO'
+        },
+        newMessage: ''
+    },
+    methods: {
+        addMessage: function () {
+            if (this.newMessage.trim().length != 0) {
+                this.items.push({message: this.newMessage});
+            }
+        }
     }
 });
