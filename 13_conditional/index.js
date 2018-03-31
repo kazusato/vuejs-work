0d232a042f6e3fdcef094e8d2d3c8e4a0db11989
sato <super80@gmail.com>
2018-03-31 13:15:56 +0900

    modified 13_conditional to add a key case and a v-show case

--- 13_conditional/index.js
+++ 13_conditional/index.js
@@ -3,6 +3,30 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        radioStatus: 'yes'
+        radioStatus: 'yes',
+        loginType: 'username',
+        toggleText: 'メールアドレスでログイン',
+        loginType2: 'username',
+        toggleText2: 'メールアドレスでログイン'
+    },
+    methods: {
+        toggleLoginType: function () {
+            if (this.loginType === 'username') {
+                this.loginType = 'email';
+                this.toggleText = 'ユーザー名でログイン';
+            } else {
+                this.loginType = 'username';
+                this.toggleText = 'メールアドレスでログイン'
+            }
+        },
+        toggleLoginType2: function () {
+            if (this.loginType2 === 'username') {
+                this.loginType2 = 'email';
+                this.toggleText2 = 'ユーザー名でログイン';
+            } else {
+                this.loginType2 = 'username';
+                this.toggleText2 = 'メールアドレスでログイン'
+            }
+        }
     }
 });
