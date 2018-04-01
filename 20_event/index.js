45cf2a347ecfb04c6cd78b308e74dabff35421e5
sato <super80@gmail.com>
2018-04-01 10:04:07 +0900

    modified 20_event

--- 20_event/index.js
+++ 20_event/index.js
@@ -11,6 +11,35 @@ const vm = new Vue({
                 event.preventDefault();
             }
             this.message = message + event;
+        },
+        clickDiv1: function () {
+            console.log('outer');
+        },
+        clickDiv2: function () {
+            console.log('mid');
+        },
+        clickDiv3: function () {
+            console.log('inner');
+        },
+        clickDiv3WithStop: function (event) {
+            event.stopPropagation();
+            console.log('inner');
+        },
+        clickReset: function () {
+            console.log('reset');
+        },
+        clickResetWithStop: function (event) {
+            event.stopPropagation();
+            console.log('reset');
+        },
+        clickResetWithPreventDefault: function (event) {
+            event.preventDefault();
+            console.log('reset');
+        },
+        clickResetWithStopAndPreventDefault: function (event) {
+            event.preventDefault();
+            event.stopPropagation();
+            console.log('reset');
         }
     }
 });
